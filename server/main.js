4ce583c60681766eb838b321ea067c9c73107a42
Ryan McClure <ryanmcclure4@gmail.com>
2016-05-28 21:13:43 -0700

    removed hard-coded api key

--- server/main.js
+++ server/main.js
@@ -1,12 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/events.js';
 
+var googleApiKey = process.env.GOOGLE_API;
+
 Meteor.methods({
     getLocation:function(geo) {
         var geoCoder = new GeoCoder({
             geocoderProvider: 'google',
             httpAdapter: 'https',
-            apiKey: 'AIzaSyBS5xANK7xna9s9cO-rd1eB1lVNxmP1bLE'
+            apiKey: googleApiKey
         }); 
         return geoCoder.reverse(geo.lat,geo.lng);
     }
